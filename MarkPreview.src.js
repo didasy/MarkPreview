e5f4cde36a6bbe5cce961da3f599b200aebfb41b
Andida Syahendar <omicron.b3ta@gmail.com>
2012-11-25 23:45:15 +0700

    Merging the non-JQ version

--- MarkPreview.src.js
+++ MarkPreview.src.js
@@ -1,100 +1,101 @@
-var MarkPreview = (function($, Showdown) {
+if(window.jQuery !== undefined) {
   
-  var _obsEl, _targetEl, _event,
-      showdown = new Showdown.converter();
-  
-  return {
+  var MarkPreview = (function($, Showdown) {
     
-    observing: function(element) {
-     
-      _obsEl = $(element);
-      
-      return this;
-      
-    },
+    var _obsEl, _targetEl, _event, _triggerEl,
+        showdown = new Showdown.converter();
     
-    triggeredBy: function(e) {
+    return {
       
-      _event = e;
-      
-      return this;
-      
-    },
-    
-    renderTo: function(element) {
-      
-      _targetEl = $(element);
-      
-      return this;
-      
-    },
-    
-    render: function() {
-      
-      _obsEl.on(_event, function(e) {
+      observing: function(element) {
+       
+        _obsEl = $(element);
         
-        _targetEl.html(showdown.makeHtml(_obsEl.val()));
+        return this;
         
-      });
-     
-      return this;
-    }
+      },
+      
+      triggeredBy: function(e, element) {
+        
+        _event = e;
+        
+        _triggerEl = $(element);
+        
+        return this;
+        
+      },
+      
+      renderTo: function(element) {
+        
+        _targetEl = $(element);
+        
+        return this;
+        
+      },
+      
+      render: function() {
+        
+        _triggerEl.on(_event, function(e) {
+          
+          _targetEl.html(showdown.makeHtml(_obsEl.val()));
+          
+        });
+       
+        return this;
+      }
+      
+    };
     
-  };
+  })(jQuery, Showdown);
+
+} else {
   
-})(jQuery, Showdown);
+  var MarkPreview = (function(Showdown) {
+  
+    var _obsEl, _targetEl, _event, _triggerEl,
+        showdown = new Showdown.converter();
+    
+    return {
+      
+      observing: function(element) {
+       
+        _obsEl = document.querySelector(element);
+        
+        return this;
+        
+      },
+      
+      triggeredBy: function(e, element) {
+        
+        _event = e;
+        
+        _triggerEl = document.querySelector(element);
+        
+        return this;
+        
+      },
+      
+      renderTo: function(element) {
+        
+        _targetEl = document.querySelector(element);
+        
+        return this;
+        
+      },
+      
+      render: function() {
+  
+        _triggerEl.addEventListener(_event, function(e) {
+          
+          _targetEl.innerHTML = showdown.makeHtml(_obsEl.value);
+          
+        }, false);
+       
+        return this;
+      }
+      
+    };
+    
+  })(Showdown);
 
-/*
-
-Created by Jesus Islam with MIT License
-
-Dependency : JQuery, Showdown.
-
-******** USAGE EXAMPLE ***********
-
-suppose the html is
-
-<!DOCTYPE html>
-<html>
-<head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-  <script src="https://github.com/coreyti/showdown/raw/master/src/showdown.js"></script>
-<meta charset=utf-8 />
-<title>JS Bin</title>
-</head>
-<body>
-  <textarea></textarea>
-  <div id="preview"></div>
-</body>
-</html>
-
-Then we can make a preview by doing this: 
-
-var makePreview = MarkPreview;
-
-makePreview.observing('textarea').triggeredBy('keyup').renderTo('#preview').render();
-
-Friggin easy
-
-
-API :
-
-.observing(element)
-
-element -> an element you wish to observe. e.g: 'textarea', '#text', etc.
-
-.triggeredBy(event, element)
-
-event -> an event to trigger MarkPreview. e.g: 'keyup', 'keydown', 'onclick', etc.
-element -> an element to trigger the event. e.g: 'button', '#previewButton', etc.
-
-.renderTo(element)
-element -> a target element for MarkPreview. e.g: '#preview', 'div.preview', etc.
-
-.render()
-Apply this method after you've done the configuration above.
-
-
-see http://jsbin.com/uterud/9/ for live demo
-
-*/
+}
